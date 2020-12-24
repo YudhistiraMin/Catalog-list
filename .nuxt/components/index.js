ee13a2805ba8857a04d3fd055e08f8aabe82e7e8
yudhistira <yudhistira_offic@gmail.com>
2020-12-24 22:49:35 +0700

    make it better

---
+++ .nuxt/components/index.js
@@ -0,0 +1,9 @@
+export { default as Footer } from '../..\\components\\Footer.vue'
+export { default as Header } from '../..\\components\\Header.vue'
+export { default as LoadingBar } from '../..\\components\\LoadingBar.vue'
+export { default as Logo } from '../..\\components\\Logo.vue'
+
+export const LazyFooter = import('../..\\components\\Footer.vue' /* webpackChunkName: "components_Footer" */).then(c => c.default || c)
+export const LazyHeader = import('../..\\components\\Header.vue' /* webpackChunkName: "components_Header" */).then(c => c.default || c)
+export const LazyLoadingBar = import('../..\\components\\LoadingBar.vue' /* webpackChunkName: "components_LoadingBar" */).then(c => c.default || c)
+export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components_Logo" */).then(c => c.default || c)
